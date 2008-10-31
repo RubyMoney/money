cb8f9aa7c0ef2b689a36785757bb0202db5bac2a
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-10-31 15:19:34 +0100

    Add unit tests.

---
+++ test/exchange_bank_spec.rb
@@ -0,0 +1,45 @@
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__) + "/../lib")
+require 'bank/variable_exchange_bank'
+
+describe VariableExchangeBank do
+	before :each do
+		@bank = VariableExchangeBank.new
+	end
+	
+	it "returns the previously specified conversion rate" do
+		@bank.add_rate("USD", "EUR", 0.788332676)
+		@bank.add_rate("EUR", "YEN", 122.631477)
+		@bank.get_rate("USD", "EUR").should == 0.788332676
+		@bank.get_rate("EUR", "YEN").should == 122.631477
+	end
+	
+	it "treats currency names case-insensitively" do
+		@bank.add_rate("usd", "eur", 1)
+		@bank.get_rate("USD", "EUR").should == 1
+		@bank.same_currency?("USD", "usd").should be_true
+		@bank.same_currency?("EUR", "usd").should be_false
+	end
+	
+	it "returns nil if the conversion rate is unknown" do
+		@bank.get_rate("American Pesos", "EUR").should be_nil
+	end
+	
+	it "exchanges money from one currency to another according to the specified conversion rates" do
+		@bank.add_rate("USD", "EUR", 0.5)
+		@bank.add_rate("EUR", "YEN", 10)
+		@bank.exchange(10_00, "USD", "EUR").should == 5_00
+		@bank.exchange(500_00, "EUR", "YEN").should == 5000_00
+	end
+	
+	it "rounds the exchanged result down" do
+		@bank.add_rate("USD", "EUR", 0.788332676)
+		@bank.add_rate("EUR", "YEN", 122.631477)
+		@bank.exhange(10_00, "USD", "EUR").should == 788
+		@bank.exhange(500_00, "EUR", "YEN").should == 6131573
+	end
+	
+	it "raises Money::UnknownRate upon conversion if the conversion rate is unknown" do
+		block = lambda { @bank.convert(10, "USD", "EUR") }
+		block.should raise_error(Money::UnknownRate)
+	end
+end
