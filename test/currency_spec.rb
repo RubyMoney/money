a904cd4614536a96b95afa37e729ff816103a898
Shane Emmons <semmons99@gmail.com>
2010-06-24 10:41:02 -0400

    add :separator to Currency Hash

--- test/currency_spec.rb
+++ test/currency_spec.rb
@@ -10,14 +10,15 @@ describe Money::Currency do
 
   specify "#initialize should lookup data from TABLE" do
     with_custom_definitions do
-      Money::Currency::TABLE[:usd] = { :priority =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100   }
-      Money::Currency::TABLE[:eur] = { :priority =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100   }
+      Money::Currency::TABLE[:usd] = { :priority =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
+      Money::Currency::TABLE[:eur] = { :priority =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
 
       currency = Money::Currency.new("USD")
       currency.id.should        == :usd
       currency.priority.should  == 1
       currency.iso_code.should  == "USD"
       currency.name.should      == "United States Dollar"
+      currency.separator.should == "."
     end
   end
 
@@ -47,14 +48,14 @@ describe Money::Currency do
 
   specify "#inspect" do
     Money::Currency.new(:usd).inspect.should ==
-    %Q{#<Money::Currency id: usd priority: 1, iso_code: USD, name: United States Dollar, symbol: $, subunit: Cent, subunit_to_unit: 100>}
+    %Q{#<Money::Currency id: usd priority: 1, iso_code: USD, name: United States Dollar, symbol: $, subunit: Cent, subunit_to_unit: 100, separator: .>}
   end
 
 
   specify "#self.find should return currency matching given id" do
     with_custom_definitions do
-      Money::Currency::TABLE[:usd] = { :priority =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100   }
-      Money::Currency::TABLE[:eur] = { :priority =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100   }
+      Money::Currency::TABLE[:usd] = { :priority =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
+      Money::Currency::TABLE[:eur] = { :priority =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
 
       expected = Money::Currency.new(:eur)
       Money::Currency.find(:eur).should  == expected
@@ -66,8 +67,8 @@ describe Money::Currency do
 
   specify "#self.find should return nil unless currency matching given id" do
     with_custom_definitions do
-      Money::Currency::TABLE[:usd] = { :position =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100   }
-      Money::Currency::TABLE[:eur] = { :position =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100   }
+      Money::Currency::TABLE[:usd] = { :position =>   1, :iso_code => "USD", :name => "United States Dollar",                      :symbol => "$",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
+      Money::Currency::TABLE[:eur] = { :position =>   2, :iso_code => "EUR", :name => "Euro",                                      :symbol => "€",             :subunit => "Cent",          :subunit_to_unit => 100, :separator => "."   }
 
       expected = Money::Currency.new(:eur)
       Money::Currency.find(:eur).should  == expected
