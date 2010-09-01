d0fb3bce8f8955bf6ce2f1d8eeea0eee7896b17d
Shane Emmons <semmons99@gmail.com>
2010-09-01 15:31:52 -0400

    update documentation for bank/base.rb

--- lib/money/bank/base.rb
+++ lib/money/bank/base.rb
@@ -1,6 +1,8 @@
 require 'thread'
 
 class Money
+  # Provides classes that aid in the ability of exchange one currency with
+  # another.
   module Bank
 
     # The lowest Money::Bank error class.
@@ -14,37 +16,59 @@ class Money
     end
 
 
+    # Money::Bank::Base is the basic interface for creating a money exchange
+    # object, also called Bank.
     #
-    # Money::Bank::Base is the basic interface for creating a money exchange object,
-    # also called Bank.
+    # A Bank is responsible for storing exchange rates, take a Money object as
+    # input and returns the corresponding Money object converted into an other
+    # currency.
     #
-    # A Bank is responsible for storing exchange rates,
-    # take a Money object as input and returns the corresponding Money object
-    # converted into an other currency.
+    # This class exists for aiding in the creating of other classes to exchange
+    # money between different currencies. When creating a subclass you will
+    # need to implement the following methods to exchange money between
+    # currencies:
     #
-    # This class exists for aiding in the creating of other classes to exchange money between
-    # different currencies. When creating a subclass you will need to implement
-    # the following  methods to exchange money between currencies:
-    #
-    # * #exchange_with(Money) # => Money
+    # - #exchange_with(Money) #=> Money
     #
     # See Money::Bank::VariableExchange for a real example.
     #
-    # Also, you can extend Money::Bank::VariableExchange
-    # instead of Money::Bank::Base if your bank implementation
-    # needs to store rates internally.
+    # Also, you can extend +Money::Bank::VariableExchange+ instead of
+    # +Money::Bank::Base+ if your bank implementation needs to store rates
+    # internally.
     #
+    # @abstract Subclass and override +#exchange_with+ to implement a custom
+    #  +Money::Bank+ class. You can also override +#setup+ instead of
+    #  +#initialize+ to setup initial variables, etc.
     class Base
 
       # Returns the singleton instance of the Base bank.
+      #
+      # @return [Money::Bank::Base]
       def self.instance
         @@singleton ||= self.new
       end
 
+      # The rounding method to use when exchanging rates.
+      #
+      # @return [Proc]
       attr_reader :rounding_method
 
-      # Initializes a new Money::Bank::Base object. An optional block can be
-      # passed to dictate the rounding method that +exchange_with+ can use.
+      # Initializes a new +Money::Bank::Base+ object. An optional block can be
+      # passed to dictate the rounding method that +#exchange_with+ can use.
+      #
+      # @yield [n] Optional block to use when rounding after exchanging one
+      #  currency for another.
+      # @yieldparam [Float] n The resulting float after exchanging one currency
+      #  for another.
+      # @yieldreturn [Integer]
+      #
+      # @return [Money::Bank::Base]
+      #
+      # @example
+      #   Money::Bank::Base.new #=> #<Money::Bank::Base @rounding_method=nil>
+      #   Money::Bank::Base.new {|n|
+      #     n.floor
+      #   } #=> #<Money::Bank::Base @round_method=#<Proc>>
       def initialize(&block)
         @rounding_method = block
         setup
@@ -52,14 +76,16 @@ class Money
 
       # Called after initialize. Subclasses can use this method to setup
       # variables, etc that they normally would in +#initialize+.
+      #
+      # @abstract Subclass and override +#setup+ to implement a custom
+      # +Money::Bank+ class.
+      #
+      # @return [self]
       def setup
       end
 
       # @deprecated +#exchange+ will be removed in v3.2.0, use +#exchange_with+
-      #
-      # Exchanges the given amount of cents in +from_currency+ to +to_currency+.
-      #
-      # Returns the amount of cents in +to_currency+ as an integer, rounded down.
+      # @see Money::Bank::Base#exchange_with
       def exchange(cents, from_currency, to_currency, &block)
         Money.deprecate "`Money::Bank::Base#exchange' will be removed in v3.2.0, use #exchange_with instead"
         exchange_with(Money.new(cents, from_currency), to_currency, &block).cents
@@ -68,30 +94,45 @@ class Money
       # Exchanges the given +Money+ object to a new +Money+ object in
       # +to_currency+.
       #
-      # You should implement this in a subclass,
-      # otherwise it will throw a NotImplementedError as a reminder.
+      # @abstract Subclass and override +#exchange_with+ to implement a custom
+      # +Money::Bank+ class.
       #
-      # Returns a new +Money+ object.
-      # Raises <tt>NotImplementedError</tt>.
+      # @raise NotImplementedError
+      #
+      # @param [Money] from The +Money+ object to exchange from.
+      # @param [Money::Currency, String, Symbol] to_currency The currency
+      #  string or object to exchange to.
+      # @yield [n] Optional block to use to round the result after making
+      #  the exchange.
+      # @yieldparam [Float] n The result after exchanging from one currency to
+      #  the other.
+      # @yieldreturn [Integer]
+      #
+      # @return [Money]
       def exchange_with(from, to_currency, &block)
         raise NotImplementedError, "#exchange_with must be implemented"
       end
 
 
-      # Given two currency strings or object,
-      # checks whether they're both the same currency.
+      # Given two currency strings or object, checks whether they're both the
+      # same currency. Return +true+ if the currencies are the same, +false+
+      # otherwise.
       #
-      #   same_currency?("usd", "USD")                # => true
-      #   same_currency?("usd", "EUR")                # => false
-      #   same_currency?("usd", Currency.new("USD")   # => true
-      #   same_currency?("usd", "USD")   # => true
+      # @param [Money::Currency, String, Symbol] currency1 The first currency
+      #  to compare.
+      # @param [Money::Currency, String, Symbol] currency2 The second currency
+      #  to compare.
       #
-      # Return +true+ if the currencies are the same, +false+ otherwise.
+      # @return [Boolean]
+      #
+      # @example
+      #   same_currency?("usd", "USD")                #=> true
+      #   same_currency?("usd", "EUR")                #=> false
+      #   same_currency?("usd", Currency.new("USD")   #=> true
+      #   same_currency?("usd", "USD")                #=> true
       def same_currency?(currency1, currency2)
         Currency.wrap(currency1) == Currency.wrap(currency2)
       end
-
     end
-
   end
 end
