bc7805a7add86846c5ffeddfc3c13ee522997f32
Shane Emmons <semmons99@gmail.com>
2010-09-01 12:03:35 -0400

    update documentation for currency.rb

--- lib/money/currency.rb
+++ lib/money/currency.rb
@@ -6,26 +6,23 @@ class Money
   class Currency
     include Comparable
 
+    # Thrown when an unknown currency is requested.
     class UnknownCurrency < StandardError; end
 
     # List of attributes applicable to a currency object.
-    #  * priority: a numerical value you can use to sort/group the currency list
-    #  * iso_code: the international 3-letter code as defined by the ISO 4217 standard
-    #  * name: the currency name
-    #  * symbol: the currency symbol (UTF-8 encoded)
-    #  * subunit: the name of the fractional monetary unit
-    #  * subunit_to_unit: the proportion between the unit and the subunit
     ATTRIBUTES = [ :priority, :iso_code, :name, :symbol, :subunit, :subunit_to_unit, :separator, :delimiter ]
 
+    # List of known currencies.
+    #
+    # == monetary unit
+    # The standard unit of value of a currency, as the dollar in the United States or the peso in Mexico.
+    # http://www.answers.com/topic/monetary-unit
+    # == fractional monetary unit, subunit
+    # A monetary unit that is valued at a fraction (usually one hundredth) of the basic monetary unit
+    # http://www.answers.com/topic/fractional-monetary-unit-subunit
+    #
+    # See http://en.wikipedia.org/wiki/List_of_circulating_currencies
     TABLE = {
-      # monetary unit
-      #   The standard unit of value of a currency, as the dollar in the United States or the peso in Mexico.
-      #   http://www.answers.com/topic/monetary-unit
-      # fractional monetary unit, subunit
-      #   A monetary unit that is valued at a fraction (usually one hundredth) of the basic monetary unit
-      #   http://www.answers.com/topic/fractional-monetary-unit-subunit
-      #
-      # See http://en.wikipedia.org/wiki/List_of_circulating_currencies
       :aed => { :priority => 100, :iso_code => "AED", :name => "United Arab Emirates Dirham",               :symbol => "د.إ",           :subunit => "Fils",          :subunit_to_unit => 100, :separator => ".", :delimiter => ","   },
       :afn => { :priority => 100, :iso_code => "AFN", :name => "Afghan Afghani",                            :symbol => "؋",             :subunit => "Pul",           :subunit_to_unit => 100, :separator => ".", :delimiter => ","   },
       :all => { :priority => 100, :iso_code => "ALL", :name => "Albanian Lek",                              :symbol => "L",             :subunit => "Qintar",        :subunit_to_unit => 100, :separator => ".", :delimiter => ","   },
@@ -194,8 +191,61 @@ class Money
       :yen => { :priority => 100, :iso_code => "JPY", :name => "Japanese Yen",                              :symbol => "¥",             :subunit => "Sen",           :subunit_to_unit => 100, :separator => ".", :delimiter => ","   },
     }
 
-    attr_reader :id, *ATTRIBUTES
+    # The symbol used to identify the currency, usually the lowercase
+    # +iso_code+ attribute.
+    #
+    # @return [Symbol]
+    attr_reader :id
 
+    # A numerical value you can use to sort/group the currency list.
+    #
+    # @return [Integer]
+    attr_reader :priority
+
+    # The international 3-letter code as defined by the ISO 4217 standard.
+    #
+    # @return [String]
+    attr_reader :iso_code
+
+    # The currency name.
+    #
+    # @return [String]
+    attr_reader :name
+
+    # The currency symbol (UTF-8 encoded).
+    #
+    # @return [String]
+    attr_reader :symbol
+
+    # The name of the fractional monetary unit.
+    #
+    # @return [String]
+    attr_reader :subunit
+
+    # The proportion between the unit and the subunit
+    #
+    # @return [Integer]
+    attr_reader :subunit_to_unit
+
+    # The character used to separate the whole unit from the subunit.
+    #
+    # @return [String]
+    attr_reader :separator
+
+    # The character used to separate thousands grouping of the whole unit.
+    #
+    # @return [String]
+    attr_reader :delimiter
+
+    # Create a new +Currency+ object.
+    #
+    # @param [String, Symbol, #to_s] id Used to look into +TABLE+ and retrieve
+    #  the applicable attributes.
+    #
+    # @return [Money::Currency]
+    #
+    # @example
+    #   Money::Currency.new(:usd) #=> #<Money::Currency id: usd ...>
     def initialize(id)
       @id  = id.to_s.downcase.to_sym
       data = TABLE[@id] || raise(UnknownCurrency, "Unknown currency `#{id}'")
@@ -204,74 +254,110 @@ class Money
       end
     end
 
-    # Compares <tt>self</tt> with <tt>other_currency</tt>
-    # against the value of <tt>priority</tt> attribute.
+    # Compares +self+ with +other_currency+ against the value of +priority+
+    # attribute.
+    #
+    # @param [Money::Currency] other_currency The currency to compare to.
+    #
+    # @return [-1,0,1] -1 if less than, 0 is equal to, 1 if greater than
+    #
+    # @example
+    #   c1 = Money::Currency.new(:usd)
+    #   c2 = Money::Currency.new(:jpy)
+    #   c1 <=> c2 #=> 1
+    #   c2 <=> c1 #=> -1
+    #   c1 <=> c1 #=> 0
     def <=>(other_currency)
       self.priority <=> other_currency.priority
     end
 
-    # Returns <tt>true</tt> if <tt>self.iso_code</tt>
-    # is equal to <tt>other_currency.iso_code</tt>
+    # Compares +self+ with +other_currency+ and returns +true+ if the are the
+    # same or if their +id+ attributes match.
+    #
+    # @param [Money::Currency] other_currency The currency to compare to.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   c1 = Money::Currency.new(:usd)
+    #   c2 = Money::Currency.new(:jpy)
+    #   c1 == c1 #=> true
+    #   c1 == c2 #=> false
     def ==(other_currency)
       self.equal?(other_currency) ||
       self.id == other_currency.id
     end
 
-    # synonymous with #==
+    # Compares +self+ with +other_currency+ and returns +true+ if the are the
+    # same or if their +id+ attributes match.
+    #
+    # @param [Money::Currency] other_currency The currency to compare to.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   c1 = Money::Currency.new(:usd)
+    #   c2 = Money::Currency.new(:jpy)
+    #   c1.eql? c1 #=> true
+    #   c1.eql? c2 #=> false
     def eql?(other_currency)
       self == other_currency
     end
 
-    # Returns a Fixnum hash value based on the <tt>id</tt> attribute
-    # in order to use functions like & (intersection), group_by, etc.
+    # Returns a Fixnum hash value based on the +id+ attribute in order to use
+    # functions like & (intersection), group_by, etc.
     #
-    #   [Currency.new(:usd), Currency.new(:eur)] & [Currency.new(:usd)]
-    #   # => [Currency.new(:usd)]
+    # @return [Fixnum]
     #
+    # @example
+    #   Money::Currency.new(:usd).hash #=> 428936
     def hash
       id.hash
     end
 
-    # Returns a string representation
-    # corresponding to the upcase <tt>id</tt> attribute.
-    #
-    #   Currency.new(:usd).to_s
-    #   # => "USD"
-    #   Currency.new(:eur).to_s
-    #   # => "EUR"
+    # Returns a string representation corresponding to the upcase +id+
+    # attribute.
     #
     # -–
-    # DEV: id.to_s.upcase corresponds to iso_code
-    # but don't use ISO_CODE for consistency.
+    # DEV: id.to_s.upcase corresponds to iso_code but don't use ISO_CODE for consistency.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Money::Currency.new(:usd).to_s #=> "USD"
+    #   Money::Currency.new(:eur).to_s #=> "EUR"
     def to_s
       id.to_s.upcase
     end
 
     # Returns a human readable representation.
     #
-    #   #<Currency id:usd>
+    # @return [String]
     #
+    # @example
+    #   Money::Currency.new(:usd) #=> #<Currency id: usd ...>
     def inspect
       "#<#{self.class.name} id: #{id} #{ATTRIBUTES.map { |a| "#{a}: #{send(a)}" }.join(", ")}>"
     end
 
-
     def method_missing(method, *args, &block)
-      Money.deprecate "`currency' is now a Currency instance. Call `currency.to_s.#{method}' instead."
+      Money.deprecate "`currency` is now a `Currency` instance. Call `currency.to_s.#{method}' instead."
       iso_code.send(method, *args, &block)
     end
 
     class << self
 
-      # Lookup a Currency with given <tt>id</tt>
-      # an returns a <tt>Currency</tt> instance on success,
-      # <tt>nil</tt> otherwise.
+      # Lookup a currency with given +id+ an returns a +Currency+ instance on
+      # success, +nil+ otherwise.
       #
-      #   Currency.find(:eur)
-      #   # => <#Currency id: eur ...>
-      #   Currency.find(:foo)
-      #   # => nil
+      # @param [String, Symbol, #to_s] id Used to look into +TABLE+ and
+      # retrieve the applicable attributes.
       #
+      # @return [Money::Currency]
+      #
+      # @example
+      #   Money::Currency.find(:eur) #=> #<Money::Currency id: eur ...>
+      #   Money::Currency.find(:foo) #=> nil
       def find(id)
         id = id.to_s.downcase.to_sym
         if data = self::TABLE[id]
@@ -279,15 +365,19 @@ class Money
         end
       end
 
-      # Wraps the object in a Currency unless it's a Currency.
+      # Wraps the object in a +Currency+ unless it's already a +Currency+
+      # object.
       #
-      #   Currency.wrap(nil)
-      #   # => nil
-      #   Currency.wrap(Currency.new(:usd))
-      #   # => <#Currency id: usd ...>
-      #   Currency.wrap("usd")
-      #   # => <#Currency id: usd ...>
+      # @param [Object] object The object to attempt and wrap as a +Currency+
+      # object.
       #
+      # @return [Money::Currency]
+      #
+      # @example
+      #   c1 = Money::Currency.new(:usd)
+      #   Money::Currency.wrap(nil)   #=> nil
+      #   Money::Currency.wrap(c1)    #=> #<Money::Currency id: usd ...>
+      #   Money::Currency.wrap("usd") #=> #<Money::Currency id: usd ...>
       def wrap(object)
         if object.nil?
           nil
@@ -297,8 +387,6 @@ class Money
           Currency.new(object)
         end
       end
-
     end
-
   end
 end
