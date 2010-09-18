6cd2e142efcb5b1b3aa4e56e33c1aa38e5673300
Simone Carletti <weppos@weppos.net>
2010-09-18 14:35:33 +0200

    No need to change the $LOAD_PATH. RSpec already appends lib and test folders. Current /lib folder is automatically appended on runtime.

--- lib/money.rb
+++ lib/money.rb
@@ -20,7 +20,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 require 'money/money'
 require 'money/defaults'
 require 'money/core_extensions'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
-
 require "spec"
 require "money"
 require "rubygems"
