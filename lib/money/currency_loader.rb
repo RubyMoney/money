b3a9deaf3aaf2a252b533d2a922b119fc9f416f7
Steve Morris <github@stevemorris.com>
2011-09-23 16:32:52 -0700

    move json files to config folder

--- lib/money/currency_loader.rb
+++ lib/money/currency_loader.rb
@@ -1,17 +1,17 @@
 module CurrencyLoader
   extend self
 
-  # Loads and returns the currencies stored in JSON files in the data directory.
+  DATA_PATH = File.dirname(__FILE__) + '/../../config/'
+
+  # Loads and returns the currencies stored in JSON files in the config directory.
   #
   # @return [Hash]
   def load_currencies
-    data_path = File.dirname(__FILE__) + '/data/'
-
-    json = File.read(data_path + 'currency.json')
+    json = File.read(DATA_PATH + 'currency.json')
     currencies = JSON.parse(json, :symbolize_names => true)
 
     # merge the currencies kept for backwards compatibility
-    json = File.read(data_path + 'currency_bc.json')
+    json = File.read(DATA_PATH + 'currency_bc.json')
     currencies.merge!(JSON.parse(json, :symbolize_names => true))
   end
 end
