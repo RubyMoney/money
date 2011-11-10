90c029f6a1d7bea59cd95ad934097605abf0b39a
John Duff <john.duff@jadedpixel.com>
2011-11-10 10:51:24 -0500

    Force json file to be UTF8
    When running ruby using the LANG flag set to something like C (LANG=C
    rake test) parsing the json file would fail in ruby 1.9. By setting the
    encoding on the file before parsing we avoid these issues.

--- lib/money/currency_loader.rb
+++ lib/money/currency_loader.rb
@@ -10,10 +10,12 @@ module CurrencyLoader
   # @return [Hash]
   def load_currencies
     json = File.read(DATA_PATH + 'currency.json')
+    json.force_encoding(::Encoding::UTF_8) if defined?(::Encoding)
     currencies = JSON.parse(json, :symbolize_names => true)
 
     # merge the currencies kept for backwards compatibility
     json = File.read(DATA_PATH + 'currency_bc.json')
+    json.force_encoding(::Encoding::UTF_8) if defined?(::Encoding)
     currencies.merge!(JSON.parse(json, :symbolize_names => true))
   end
 end
