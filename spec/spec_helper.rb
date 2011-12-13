603e1d09a1ab2293ad208aa4d4196e6d44efcd31
Simone Carletti <weppos@weppos.net>
2011-12-13 18:10:31 +0100

    Cleanup rspec helper file.
    Assume Rspec is available, because included in the Gemfile file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
-require "money"
-require "rubygems"
+require 'rspec'
+require 'money'
 
-#RSpec.configure do |config|
-#end
+unless defined?(SPEC_ROOT)
+  SPEC_ROOT = File.expand_path("../", __FILE__)
+end
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(SPEC_ROOT, "support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+end
+
 
 def silence_warnings
   old_verbose, $VERBOSE = $VERBOSE, nil
@@ -11,18 +20,18 @@ ensure
   $VERBOSE = old_verbose
 end
 
-def reset_i18n()
+def reset_i18n
   I18n.backend = I18n::Backend::Simple.new
 end
 
 def store_number_currency_formats(locale)
   I18n.backend.store_translations(locale,
                                   :number => {
-                                    :currency => {
-                                      :format => {
-                                        :delimiter => ",",
-                                        :separator => "."
+                                      :currency => {
+                                          :format => {
+                                              :delimiter => ",",
+                                              :separator => "."
+                                          }
                                       }
-                                    }
                                   })
 end
