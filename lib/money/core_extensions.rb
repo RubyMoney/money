6137b0024c43aa9afc323784002999c240b7ca31
Andreas Loupasakis <alup@aloop.org>
2012-05-03 14:51:47 +0300

    Provide String#to_money! function which raises exception

--- lib/money/core_extensions.rb
+++ lib/money/core_extensions.rb
@@ -46,6 +46,31 @@ class String
     Money.parse(self, currency)
   end
 
+  # Parses the current string and converts it to a +Money+ object.
+  # Excess characters will be discarded. If no numeric characters are given
+  # it raises an ArgumentError.
+  #
+  # @param [Currency, String, Symbol] currency
+  #   The currency to set the resulting +Money+ object to.
+  #
+  # @return [Money]
+  #
+  # @raise [ArgumentError] If this String does not contain any numeric character.
+  #
+  # @example
+  #   '100'.to_money                #=> #<Money @cents=10000>
+  #   '100.37'.to_money             #=> #<Money @cents=10037>
+  #   '100 USD'.to_money            #=> #<Money @cents=10000, @currency=#<Money::Currency id: usd>>
+  #   'USD 100'.to_money            #=> #<Money @cents=10000, @currency=#<Money::Currency id: usd>>
+  #   '$100 USD'.to_money           #=> #<Money @cents=10000, @currency=#<Money::Currency id: usd>>
+  #   'hello 2000 world'.to_money   #=> #<Money @cents=200000 @currency=#<Money::Currency id: usd>>
+  #
+  # @see Money.from_string
+  #
+  def to_money!(currency = nil)
+    Money.parse!(self, currency)
+  end
+
   # Converts the current string into a +Currency+ object.
   #
   # @return [Money::Currency]
