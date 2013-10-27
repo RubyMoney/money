ed5a18d34828f4942bc3dd1ffdd071740d919edd
chatgris <jboyer@af83.com>
2013-10-27 11:41:57 +0900

    Allow to inherits from Money
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/money/money.rb
+++ lib/money/money.rb
@@ -128,26 +128,34 @@ class Money
     attr_accessor :conversion_precision
   end
 
-  # Set the default bank for creating new +Money+ objects.
-  self.default_bank = Bank::VariableExchange.instance
+  def self.set_up_default
+    # Set the default bank for creating new +Money+ objects.
+    self.default_bank = Bank::VariableExchange.instance
 
-  # Set the default currency for creating new +Money+ object.
-  self.default_currency = Currency.new("USD")
+    # Set the default currency for creating new +Money+ object.
+    self.default_currency = Currency.new("USD")
 
-  # Default to using i18n
-  self.use_i18n = true
+    # Default to using i18n
+    self.use_i18n = true
 
-  # Default to not using currency symbol assumptions when parsing
-  self.assume_from_symbol = false
+    # Default to not using currency symbol assumptions when parsing
+    self.assume_from_symbol = false
 
-  # Default to not using infinite precision cents
-  self.infinite_precision = false
+    # Default to not using infinite precision cents
+    self.infinite_precision = false
 
-  # Default to bankers rounding
-  self.rounding_mode = BigDecimal::ROUND_HALF_EVEN
+    # Default to bankers rounding
+    self.rounding_mode = BigDecimal::ROUND_HALF_EVEN
 
-  # Default the conversion of Rationals precision to 16
-  self.conversion_precision = 16
+    # Default the conversion of Rationals precision to 16
+    self.conversion_precision = 16
+  end
+
+  def self.inherited(base)
+    base.set_up_default
+  end
+
+  set_up_default
 
   # Create a new money object with value 0.
   #
