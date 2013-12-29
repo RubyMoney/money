3871e9c1e712d0cbf697d510baeb0ecc6f4aecb3
Shane Emmons <shane@emmons.io>
2013-12-29 12:39:33 -0800

    track test coverage w/ coveralls.io

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem "coveralls", :require => false
 gem "json", :platform => [:jruby, :mri_18]
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
+require "coveralls"
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.dirname(__FILE__)
-require 'rspec'
-require 'money'
-require 'support/default_currency_helper'
+require "rspec"
+require "money"
+require "support/default_currency_helper"
 
 RSpec.configure do |c|
-  c.order = "rand"
+  c.order = :random
   c.include DefaultCurrencyHelper
 end
 
