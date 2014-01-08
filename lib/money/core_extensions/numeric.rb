7358b48befdfa33d326502c8ec2f8afe7da8772f
Saurabh Nanda <saurabh@vacationlabs.com>
2014-01-08 08:47:25 +0530

    Update numeric.rb

--- lib/money/core_extensions/numeric.rb
+++ lib/money/core_extensions/numeric.rb
@@ -2,7 +2,7 @@ class Numeric
   alias_method :_to_money, :to_money
   def to_money(*args)
     unless Money.silence_core_extensions_deprecations
-      Money.deprecate "as of Money 6.1.0 you must `require 'monetize/core_extensions'` to use Numeric#to_money."
+      Money.deprecate "as of Money 6.1.0 you must `require 'money/core_extensions'` to use Numeric#to_money."
     end
     _to_money(*args)
   end
