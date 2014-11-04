08056bcc645ac21bc1177a994cae9ec836517e0c
Lin Zhang <marshluca@gmail.com>
2014-11-04 21:59:50 +0800

    Refactor Numeric extention methods

--- lib/money/extensions/numeric.rb
+++ lib/money/extensions/numeric.rb
@@ -1,160 +1,156 @@
-class Numeric
+COIN_I2F = 100_000_000
+MONEY_I2F = 100_000
+MONEY_DELTA = 1_000
 
-  #########################################################################
+class Integer
+
+  # Convert number from big integer value to float value.
   #
-  # Money Formatter Methods (the receiver is in big integer value)
-  #
-  #  - the method starts with 'to_' returns real money with currency
-  #  - the method starts with 'pure_' returns real money without currency
-  #  - the method starts with 'raw_' returns big integer value of money
-  #
-  #########################################################################
-
-
-  MONEY_I2F = 100000
-  MONEY_DELTA = 1000
-
-
-  # Convert money from big integer value to currency value.
-  #
-  # @param [String] money_type
-  #
-  # @return [String]
-  #
-  # @example
-  #   123400000.to_money => "NT$1,234.00"
-  #   123400000.to_money('twd') => "NT$1,234.00"
-
-  def to_money(money_type = 'twd')
-    currency = Money::Currency.new money_type
-    amount = self.to_f * currency.subunit_to_unit / MONEY_I2F
-    Money.new(amount, money_type).format(disambiguate: true)
-  end
-
-
-  # Convert money from big integer value to round currency value.
-  #
-  # @param [String] money_type
-  #
-  # @return [String]
-  #
-  # @example
-  #   123426000.to_round_money('twd') => "NT$1,234"
-  #   123456000.to_round_money('twd') => "NT$1,235"
-
-  def to_round_money(money_type = 'twd')
-    currency = Money::Currency.new money_type
-    amount = self.to_f * currency.subunit_to_unit / MONEY_I2F
-    amount = (amount/currency.subunit_to_unit).round * currency.subunit_to_unit
-    Money.new(amount, money_type).format(disambiguate: true, no_cents: true)
-  end
-
-
-  # Convert money from big integer value to ceil currency value.
-  #
-  # @param [String] money_type
-  #
-  # @return [String]
-  #
-  # @example
-  #   123426000.to_ceil_money('twd') => "NT$1,235.00"
-  #   123476000.to_ceil_money('twd') => "NT$1,235.00"
-
-  def to_ceil_money(money_type = 'twd')
-    currency = Money::Currency.new money_type
-    amount = self.to_f * currency.subunit_to_unit / MONEY_I2F
-    amount = (amount/currency.subunit_to_unit).ceil * currency.subunit_to_unit
-    Money.new(amount, money_type).format(disambiguate: true)
-  end
-
-
-  # Convert money from big integer value to float value.
-  #
-  # @param [String] money_type
+  # @param [Symbol] currency
+  # @param [Hash] opts
   #
   # @return [Float]
   #
   # @example
-  #   123426000.pure_money('twd') => 1234.26
-  #   123456000.pure_money('twd') => 1234.56
+  #   123456789.to_float(:btc) => 1.23456789
+  #   123456789.to_float(:cny) => 1234.56789
 
-  def pure_money(money_type = 'twd')
+  def to_float(currency = :twd, opts = {})
+    case currency.to_sym
+    when :btc, :ltc
+      normalize_coin(currency, opts)
+    when :cny, :twd, :usd
+      normalize_money(currency, opts)
+    else
+      raise Money::Currency::UnknownCurrency, currency.inspect
+    end
+  end
+
+
+  # Convert number from big integer value to floor or ceil value.
+  #
+  # @param [Symbol] currency
+  # @param [Hash] opts
+  #
+  # @return [Integer]
+  #
+  # @example
+  #   (123426845).to_integer(:cny, ceil: true) => 1234270000
+  #   (123426345).to_integer(:twd, floor: true) => 1234260000
+
+  def to_integer(currency = :twd, opts = {})
+    case currency.to_sym
+    when :cny, :twd, :usd
+      if opts[:ceil]
+        (self.to_f / MONEY_DELTA).ceil * MONEY_DELTA
+      elsif opts[:floor]
+        (self.to_f / MONEY_DELTA).floor * MONEY_DELTA
+      end
+    else
+      raise Money::Currency::UnknownCurrency, currency.inspect
+    end
+  end
+
+
+  # Convert number from big integer value to string value.
+  #
+  # @param [Symbol] currency
+  # @param [Hash] opts
+  #
+  # @return [String]
+  #
+  # @example
+  #   123456789.to_string(:btc) => "1.23456789 BTC"
+  #   123456789.to_string(:twd) => "NT$1,234.57"
+  #   123456789.to_string(:twd, ceil: true) => "NT$1,235.00"
+  #   123456789.to_string(:twd, round: true) => "NT$1,235"
+
+  def to_string(currency = :twd, opts = {})
+    case currency.to_sym
+    when :btc, :ltc
+      stringify_coin(currency, opts)
+    when :cny, :twd, :usd
+      stringify_money(currency, opts)
+    else
+      raise Money::Currency::UnknownCurrency, currency.inspect
+    end
+  end
+
+  private
+
+  def normalize_coin(currency, opts = {})
+    self.to_f / COIN_I2F
+  end
+
+  def normalize_money(currency, opts = {})
     self.to_f / MONEY_I2F
   end
 
-
-  # Convert money from big integer value to round integer value.
-  #
-  # @param [String] money_type
-  #
-  # @return [Integer]
-  #
-  # @example
-  #   123426000.pure_round_money('twd') => 1234
-  #   123456000.pure_round_money('twd') => 1235
-
-  def pure_round_money(money_type = 'twd')
-    (self.to_f / MONEY_I2F).round
+  def stringify_coin(currency, opts = {})
+    currency_object = Money::Currency.new currency
+    amount = self.to_f * currency_object.subunit_to_unit / COIN_I2F
+    Money.new(amount, currency).format default_coin_options.merge(opts)
   end
 
+  def stringify_money(currency, opts = {})
+    currency_object = Money::Currency.new currency
+    delta = currency_object.subunit_to_unit
+    amount = self.to_f * delta / MONEY_I2F
 
-  # The floor value (big integer) of big integer money value.
-  #
-  # @param [String] money_type
-  #
-  # @return [Integer]
-  #
-  # @example
-  #   (123426345).raw_floor_money('twd') => 1234260000
-  #   (123426845).raw_floor_money('twd') => 1234260000
+    if opts[:round]
+      amount = (amount/delta).round * delta
+      opts = opts.merge(no_cents: true)
+    end
 
-  def raw_floor_money(money_type = 'twd')
-    (self.to_f / MONEY_DELTA).floor * MONEY_DELTA
+    if opts[:ceil]
+      amount = (amount/delta).ceil * delta
+    end
+
+    Money.new(amount, currency).format default_money_options.merge(opts)
   end
 
-
-  # The ceil value (big integer) of big integer money value.
-  #
-  # @param [String] money_type
-  #
-  # @return [Integer]
-  #
-  # @example
-  #   (123426345).raw_ceil_money('twd') => 1234270000
-  #   (123426845).raw_ceil_money('twd') => 1234270000
-
-  def raw_ceil_money(money_type = 'twd')
-    (self.to_f / MONEY_DELTA).ceil * MONEY_DELTA
+  def default_money_options
+    { disambiguate: true }
   end
 
-
-  # The fee (big integer) of big integer money value.
-  #
-  # @param [Float] rate
-  # @param [String] money_type
-  #
-  # @return [Integer]
-  #
-  # @example
-  #   (100_00000).raw_money_for_fee(0.3, 'twd') => 30_00000
-  #   (100_00000).raw_money_for_fee(0, 'twd') => 0
-
-  def raw_money_for_fee(rate, money_type = 'twd')
-    ((self.to_f * rate / MONEY_DELTA).ceil) * MONEY_DELTA
+  def default_coin_options
+    { symbol: false, with_currency: true }
   end
 
+end
 
-  # The big integer value of float money.
-  # It accepts a Float number as receiver.
+
+class Float
+
+  # Convert number from float value to big integer value.
   #
-  # @param [String] money_type
+  # @param [Symbol] currency
+  # @param [Hash] opts
   #
   # @return [Integer]
   #
   # @example
-  #   (1234.26).f2i_money('twd') => 123426000
+  #   (1.23456789).to_integer(:btc) => 123456789
+  #   (1.23456789).to_integer(:twd) => 123457
 
-  def f2i_money(money_type = 'twd')
+  def to_integer(currency = :twd, opts = {})
+    case currency.to_sym
+    when :btc, :ltc
+      unnormalize_coin(opts)
+    when :cny, :twd, :usd
+      unnormalize_money(opts)
+    else
+      raise Money::Currency::UnknownCurrency, currency.inspect
+    end
+  end
+
+  private
+
+  def unnormalize_coin(opts = {})
+    (self * COIN_I2F).round
+  end
+
+  def unnormalize_money(opts = {})
     (self * MONEY_I2F).round
   end
 
