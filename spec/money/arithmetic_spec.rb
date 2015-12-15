377e48f4a09e93a5ef6e861d43cd1abc3bde80b4
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2015-12-15 12:42:07 -0500

    Update <=> to return nil if the comparison is inappropriate
    Comparable#== no more rescue exceptions of #<=> in ruby >= 2.3
    
    @see https://bugs.ruby-lang.org/issues/7688

--- spec/money/arithmetic_spec.rb
+++ spec/money/arithmetic_spec.rb
@@ -109,25 +109,15 @@ describe Money do
     end
 
     it "raises TypeError when used to compare with an object that doesn't inherit from Money" do
-      expect {
-        Money.new(1_00) <=> 100
-      }.to raise_exception(TypeError)
+      expect(Money.new(1_00) <=> 100).to be_nil
 
-      expect {
-        Money.new(1_00) <=> Object.new
-      }.to raise_exception(TypeError)
+      expect(Money.new(1_00) <=> Object.new).to be_nil
 
-      expect {
-        Money.new(1_00) <=> Class
-      }.to raise_exception(TypeError)
+      expect(Money.new(1_00) <=> Class).to be_nil
 
-      expect {
-        Money.new(1_00) <=> Kernel
-      }.to raise_exception(TypeError)
+      expect(Money.new(1_00) <=> Kernel).to be_nil
 
-      expect {
-        Money.new(1_00) <=> /foo/
-      }.to raise_exception(TypeError)
+      expect(Money.new(1_00) <=> /foo/).to be_nil
     end
   end
 
