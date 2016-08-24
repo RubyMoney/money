61035e4eacb6c1e5eb993d44f17409f4e3f64773
Webber Lee <webber@maicoin.com>
2016-08-24 10:32:09 +0800

    Remove excessive whitespace

--- lib/money/extensions/string.rb
+++ lib/money/extensions/string.rb
@@ -1,5 +1,4 @@
 class String
-
   # Get the name of currency
   #
   # @param [String] currency
@@ -8,7 +7,6 @@ class String
   #
   # @example
   #   'btc'.to_name => 'BTC'
-
   def to_name
     case self.to_s
     when 'btc', 'eth'
@@ -21,7 +19,6 @@ class String
     end
   end
 
-
   # Get the symbol of currency
   #
   # @param
@@ -32,9 +29,7 @@ class String
   #   'cny'.to_symbol => '¥'
   #   'twd'.to_symbol => 'NT$'
   #   'usd'.to_symbol => '$'
-
   def to_symbol
     Money::Currency.new(self).disambiguate_symbol || Money::Currency.new(self).symbol
   end
-
 end
