fc4074f7f669ae3c0e2e9be9dd6a0adbf822e0ba
Webber Lee <webber@maicoin.com>
2016-08-24 11:00:25 +0800

    Improve string_spec.rb

--- spec/extensions/string_spec.rb
+++ spec/extensions/string_spec.rb
@@ -3,20 +3,17 @@
 require "spec_helper"
 
 describe Money, "extension" do
-
-  describe "#to_coin_name" do
-    it "converts string to coin name" do
+  describe "#to_currency_name" do
+    it "converts string to currency name" do
       expect('btc'.to_currency_name).to eq 'Bitcoin'
       expect('eth'.to_currency_name).to eq 'Ether'
-      expect { 'ltc'.to_currency_name } .to raise_error Money::Currency::UnknownCurrency
-    end
-  end
-
-  describe "#to_money_name" do
-    it "converts string to money name" do
+      expect('twd'.to_currency_name).to eq 'New Taiwan Dollar'
       expect('cny'.to_currency_name).to eq 'Chinese Renminbi Yuan'
       expect('usd'.to_currency_name).to eq 'United States Dollar'
-      expect('twd'.to_currency_name).to eq 'New Taiwan Dollar'
+    end
+
+    it "raises UnknownCurrency with unknown currency" do
+      expect { 'ltc'.to_currency_name } .to raise_error Money::Currency::UnknownCurrency
     end
   end
 
@@ -33,12 +30,17 @@ describe Money, "extension" do
     end
   end
 
-  describe "#to_money_symbol" do
-    it "converts string to money symbol" do
+  describe "#to_currency_symbol" do
+    it "converts string to currency symbol" do
+      expect('btc'.to_currency_symbol).to eq 'B⃦'
+      expect('eth'.to_currency_symbol).to eq 'Æ'
+      expect('twd'.to_currency_symbol).to eq 'NT$'
       expect('cny'.to_currency_symbol).to eq '¥'
       expect('usd'.to_currency_symbol).to eq '$'
-      expect('twd'.to_currency_symbol).to eq 'NT$'
+    end
+
+    it "raises UnknownCurrency with unknown currency" do
+      expect { 'ltc'.to_currency_symbol } .to raise_error Money::Currency::UnknownCurrency
     end
   end
-
 end
