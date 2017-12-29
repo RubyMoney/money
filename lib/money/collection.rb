91267edb0ea73baaff1357d3daf751fd126b3469
lulalala <mark@goodlife.tw>
2017-12-29 11:45:55 +0800

    Basic implementation of sum()

--- lib/money/collection.rb
+++ lib/money/collection.rb
@@ -1,4 +1,60 @@
 class Money
   class Collection
+    include Enumerable
+
+    # @parma array [#to_a] collection of Money objects
+    def initialize(array = nil)
+      @collection = array.to_a
+    end
+
+    # Sums up Money objects in collection.
+    # @param target_currency [Currency, String, Symbol] - currency of the returning money object.
+    # @return [Money] sum of Money collection.
+    def sum(target_currency = nil)
+      if @collection.empty?
+        return Money.new(0, target_currency)
+      end
+
+      moneys_by_currency = @collection.group_by{|money|
+        money.currency
+      }.values
+
+      sum = self.class.sum_basic(moneys_by_currency.map{|moneys| self.class.sum_basic(moneys)})
+      if target_currency.nil?
+        sum
+      else
+        sum.exchange_to(target_currency)
+      end
+    end
+
+    #### delegations
+
+    def each
+      if block_given?
+        @collection.each{|x| yield(x)}
+        self
+      else
+        @collection.each
+      end
+    end
+
+    def <<(obj)
+      @collection << obj
+      self
+    end
+
+    def concat(other_ary)
+      @collection.concat(other_ary)
+      self
+    end
+
+    private
+
+    # Sums up Money objects using built-in :+ method.
+    # @param moneys [Enumerable<Money>] list of Moneys.
+    # @return [Money] sum of Money collection.
+    def self.sum_basic(moneys)
+      moneys.reduce{|total, money| total + money }
+    end
   end
 end
---
+++ spec/collection_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+require 'money/collection'
+
+RSpec.describe Money::Collection do
+  # Adds up array of Money one by one and returns the sum.
+  def normal_sum(array)
+    array.reduce{|total, money| total + money }
+  end
+
+  before :all do
+    foo = {
+      :iso_code        => "FOO",
+      :subunit_to_unit => 100,
+    }
+    Money::Currency.register(foo)
+
+    Money.add_rate("FOO", "USD", 0.5)
+    Money.add_rate("USD", "FOO", 2)
+  end
+
+  after :all do
+    Money::Currency.unregister("FOO")
+  end
+
+  describe '#sum' do
+    it 'sums with no element' do
+      c = Money::Collection.new
+      expect(c.sum).to eq(Money.zero)
+    end
+
+    it 'sums with single element' do
+      c = Money::Collection.new
+      c << Money.new(10,:usd)
+      expect(c.sum).to eq(Money.new(10,:usd))
+    end
+
+    it 'sums same currency' do
+      ary = [
+        Money.new(10,:usd),
+        Money.new(20,:usd),
+        Money.new(30,:usd),
+      ]
+
+      c = Money::Collection.new(ary)
+
+      expect(c.sum).to eq(Money.new(60,:usd))
+    end
+
+    it 'sums different currencies' do
+      ary = [
+        Money.new(100,:usd),
+        Money.new(10,:foo),
+      ]
+
+      c = Money::Collection.new(ary)
+
+      expect(c.sum).to eq(normal_sum(ary))
+    end
+
+    it 'sums correctly, avoiding rounding down error' do
+      ary = [
+        Money.new(10,:usd),
+        Money.new(1,:foo),
+        Money.new(1,:foo),
+      ]
+
+      c = Money::Collection.new(ary)
+
+      expect(c.sum).to eq(Money.new(11,:usd))
+    end
+
+    it 'returns sum in the specified currency' do
+      ary = [
+        Money.new(10,:usd),
+        Money.new(2,:foo),
+      ]
+
+      c = Money::Collection.new(ary)
+
+      expect(c.sum('foo')).to eq(Money.new(22,:foo))
+      expect(c.sum('usd')).to eq(Money.new(11,:usd))
+    end
+  end
+
+  describe '#concat' do
+    it 'concats Money objects to collection' do
+      ary = [
+        Money.new(10,:usd),
+        Money.new(2,:foo),
+      ]
+
+      c = Money::Collection.new
+      c.concat ary
+
+      expect(c.sum('foo')).to eq(Money.new(22,:foo))
+    end
+  end
+end
