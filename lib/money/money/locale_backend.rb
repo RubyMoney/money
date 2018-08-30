d69cca7cc783a301c58380c9bfbe9aa79f736f71
Alexander Popov <alex.wayfer@gmail.com>
2018-08-30 11:30:41 +0300

    Add R18n locale backend
    https://github.com/ai/r18n

--- lib/money/money/locale_backend.rb
+++ lib/money/money/locale_backend.rb
@@ -3,12 +3,14 @@
 require 'money/locale_backend/errors'
 require 'money/locale_backend/legacy'
 require 'money/locale_backend/i18n'
+require 'money/locale_backend/r18n'
 
 class Money
   module LocaleBackend
     BACKENDS = {
       legacy: Money::LocaleBackend::Legacy,
-      i18n: Money::LocaleBackend::I18n
+      i18n: Money::LocaleBackend::I18n,
+      r18n: Money::LocaleBackend::R18n
     }.freeze
 
     def self.find(name)
