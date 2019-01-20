7bcf4b03fe47d96ea9fde14844f83b583d715fcb
Anthony Dmitriyev <antstorm@gmail.com>
2019-01-20 22:36:38 +0000

    Use Currency::Loader directly without extending

--- lib/money/currency/loader.rb
+++ lib/money/currency/loader.rb
@@ -3,21 +3,23 @@ class Money
     module Loader
       DATA_PATH = File.expand_path("../../../../config", __FILE__)
 
-      # Loads and returns the currencies stored in JSON files in the config directory.
-      #
-      # @return [Hash]
-      def load_currencies
-        currencies = parse_currency_file("currency_iso.json")
-        currencies.merge! parse_currency_file("currency_non_iso.json")
-        currencies.merge! parse_currency_file("currency_backwards_compatible.json")
-      end
+      class << self
+        # Loads and returns the currencies stored in JSON files in the config directory.
+        #
+        # @return [Hash]
+        def load_currencies
+          currencies = parse_currency_file("currency_iso.json")
+          currencies.merge! parse_currency_file("currency_non_iso.json")
+          currencies.merge! parse_currency_file("currency_backwards_compatible.json")
+        end
 
-      private
+        private
 
-      def parse_currency_file(filename)
-        json = File.read("#{DATA_PATH}/#{filename}")
-        json.force_encoding(::Encoding::UTF_8) if defined?(::Encoding)
-        JSON.parse(json, symbolize_names: true)
+        def parse_currency_file(filename)
+          json = File.read("#{DATA_PATH}/#{filename}")
+          json.force_encoding(::Encoding::UTF_8) if defined?(::Encoding)
+          JSON.parse(json, symbolize_names: true)
+        end
       end
     end
   end
--- spec/currency/loader_spec.rb
+++ spec/currency/loader_spec.rb
@@ -1,19 +1,13 @@
 # encoding: utf-8
 
 describe Money::Currency::Loader do
-  class CurrencyLoader
-    include Money::Currency::Loader
-  end
-
-  let(:loader) { CurrencyLoader.new }
-
   it "returns a currency table hash" do
-    expect(loader.load_currencies).to be_a Hash
+    expect(subject.load_currencies).to be_a Hash
   end
 
   it "parse currency_iso.json & currency_non_iso.json & currency_backwards_compatible.json" do
-    expect(loader).to receive(:parse_currency_file).exactly(3).times.and_return({})
+    expect(subject).to receive(:parse_currency_file).exactly(3).times.and_return({})
 
-    loader.load_currencies
+    subject.load_currencies
   end
 end
