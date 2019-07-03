dd12f29b84d76310ec1b666c44ea8aca77574af4
Anthony Dmitriyev <antstorm@gmail.com>
2019-07-03 22:01:47 +0100

    Exclude i18n specs from rbx suite (#878)
    * Allow running specs without I18n
    
    * Exclude i18n specs from rbx
    
    * Disallow rbx failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,21 +5,21 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require "rspec"
 require "money"
 
-require "i18n"
 require "r18n-core"
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
-
-I18n.enforce_available_locales = false
 
 RSpec.configure do |c|
   c.order = :random
   c.filter_run :focus
   c.run_all_when_everything_filtered = true
+  c.filter_run_excluding :i18n if ENV['I18N'] == 'false'
 end
 
 RSpec.shared_context 'with i18n locale backend', :i18n do
   around do |example|
+    require 'i18n'
+    I18n.enforce_available_locales = false
     Money.locale_backend = :i18n
 
     example.run
