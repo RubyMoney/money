bb06f68d367e9b5c1fbb9e5ccda5578737d2259c
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-04-23 12:15:41 +0200

    Remove redundant encoding
    This magic comment is not needed anymore because the default encoding of
    source code files assumed by Ruby 2 is utf-8, and the minimum required
    Ruby version is 3.1
    
    Autofixed with:
    ```
    rubocop -a --only Style/Encoding
    ```

--- spec/money/formatting_spec.rb
+++ spec/money/formatting_spec.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 describe Money, "formatting" do
 
   BAR = '{ "priority": 1, "iso_code": "BAR", "iso_numeric": "840", "name": "Dollar with 4 decimal places", "symbol": "$", "subunit": "Cent", "subunit_to_unit": 10000, "symbol_first": true, "html_entity": "$", "decimal_mark": ".", "thousands_separator": ",", "smallest_denomination": 1 }'
