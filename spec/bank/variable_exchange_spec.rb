747daacd582e60393887fa52f619297a149e38f5
Sunny Ripert <sunny@sunfox.org>
2025-11-04 01:03:04 +0100

    Style/Proc

--- spec/bank/variable_exchange_spec.rb
+++ spec/bank/variable_exchange_spec.rb
@@ -66,7 +66,7 @@ RSpec.describe Money::Bank::VariableExchange do
         #end
 
         it "accepts a custom truncation method" do
-          proc = Proc.new { |n| n.ceil }
+          proc = proc { |n| n.ceil }
           expect(bank.exchange_with(Money.new(10, 'USD'), 'EUR', &proc)).to eq Money.new(14, 'EUR')
         end
 
@@ -88,7 +88,7 @@ RSpec.describe Money::Bank::VariableExchange do
 
     context "with &block" do
       let(:bank) {
-        proc = Proc.new { |n| n.ceil }
+        proc = proc { |n| n.ceil }
         described_class.new(&proc).tap do |bank|
           bank.add_rate('USD', 'EUR', 1.33)
         end
@@ -100,7 +100,7 @@ RSpec.describe Money::Bank::VariableExchange do
         end
 
         it "accepts a custom truncation method" do
-          proc = Proc.new { |n| n.ceil + 1 }
+          proc = proc { |n| n.ceil + 1 }
           expect(bank.exchange_with(Money.new(10, 'USD'), 'EUR', &proc)).to eq Money.new(15, 'EUR')
         end
       end
