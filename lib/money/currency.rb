3b952bcdaa490b798a51f310264ed0cc7a06d6ea
Sunny Ripert <sunny@sunfox.org>
2025-11-04 09:26:04 +0100

    Layout/LineContinuationSpacing

--- lib/money/currency.rb
+++ lib/money/currency.rb
@@ -23,7 +23,7 @@ class Money
     class MissingAttributeError < StandardError
       def initialize(method, currency, attribute)
         super(
-          "Can't call Currency.#{method} - currency '#{currency}' is missing "\
+          "Can't call Currency.#{method} - currency '#{currency}' is missing " \
           "the attribute '#{attribute}'"
         )
       end
