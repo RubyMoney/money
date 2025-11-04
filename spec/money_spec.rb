a597ecea5e0633b212d2bc5786ffbe79c6d811cc
Sunny Ripert <sunny@sunfox.org>
2025-11-04 01:39:28 +0100

    Layout/HeredocIndentation

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -389,26 +389,27 @@ RSpec.describe Money do
     end
 
     context "loading a serialized Money via YAML" do
-      let(:serialized) do <<YAML
-!ruby/object:Money
-  fractional: 249.5
-  currency: !ruby/object:Money::Currency
-    id: :eur
-    priority: 2
-    iso_code: EUR
-    name: Euro
-    symbol: €
-    alternate_symbols: []
-    subunit: Cent
-    subunit_to_unit: 100
-    symbol_first: true
-    html_entity: ! '&#x20AC;'
-    decimal_mark: ! ','
-    thousands_separator: .
-    iso_numeric: '978'
-    mutex: !ruby/object:Thread::Mutex {}
-    last_updated: 2012-11-23 20:41:47.454438399 +02:00
-YAML
+      let(:serialized) do
+        <<~YAML
+          !ruby/object:Money
+            fractional: 249.5
+            currency: !ruby/object:Money::Currency
+              id: :eur
+              priority: 2
+              iso_code: EUR
+              name: Euro
+              symbol: €
+              alternate_symbols: []
+              subunit: Cent
+              subunit_to_unit: 100
+              symbol_first: true
+              html_entity: ! '&#x20AC;'
+              decimal_mark: ! ','
+              thousands_separator: .
+              iso_numeric: '978'
+              mutex: !ruby/object:Thread::Mutex {}
+              last_updated: 2012-11-23 20:41:47.454438399 +02:00
+        YAML
       end
 
       let(:m) do
